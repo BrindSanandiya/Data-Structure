1301b96b634be54e6e4f8cc7dbcea9489d0e2cf2
Brind Sanandiya <brindbsan7@gmail.com>
2024-09-24 10:46:50 +0530

    Update Linear.java

--- Lab18/Linear.java
+++ Lab18/Linear.java
@@ -1,27 +1,33 @@
-
 import java.util.Scanner;
 
-public class Linear {
+public class LinearSearch{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Size of Array : ");
+		int n = sc.nextInt();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size: ");
+		int[] arr = new int[n];
+		int index = -1;
 
-        int size = sc.nextInt();
-        int[] a = new int[size];
+		for(int i=0;i<n;i++){
+			arr[i] = sc.nextInt();
+		}
 
-        for (int i = 0; i < size; i++) {
-            System.out.print("Enter element at index "+i+": ");
-            a[i] = sc.nextInt();
-        }
-        System.out.println("Enter value to be searched");
-        int value = sc.nextInt();
+		System.out.print("Enter the value to find : ");
+		int val = sc.nextInt();
 
-        for (int i = 0; i < size; i++) {
-            if (a[i] == value) {
-                System.out.println("Value found at " + i);
-            }
-        }
+		for(int i=0;i<n;i++){
 
-    }
+			if(arr[i] == val){
+				index = i;
+				System.out.println("Index is : "+i);
+				break;
+			}
+		}
+		if(index == -1){
+			System.out.println("Value not found");
+		}
+
+	}
 }
